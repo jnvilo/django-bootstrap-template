f4afe44545a3ac3e111be53ea8730311c22cf17a
Jason Viloria <jason.viloria@optiscangroup.com>
2020-08-01 00:56:19 +0300

    added templates

--- web/bootstrap/urls.py
+++ web/bootstrap/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.views.generic import TemplateView
 from .views import IndexView
 
-class AboutView(TemplateView):
-    template_name = "about.html"
-
 urlpatterns = [
+
+    path('base', TemplateView.as_view(template_name="bootstrap/base.html")),
     path('', TemplateView.as_view(template_name="bootstrap/index.html")),
+    path('page-nav', TemplateView.as_view(template_name="bootstrap/page-nav.html")),
+
+
 ]
